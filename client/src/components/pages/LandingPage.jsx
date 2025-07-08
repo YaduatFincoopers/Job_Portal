455f50a76f491a43c38ef3fb9764d6dc2c6c7db0
yadushrivastava@fincoopers.in <shrivastavyadu@gmail.com>
2025-07-08 16:23:41 +0530

    Resolve merge conflict in LandingPage.jsx

--- client/src/components/pages/LandingPage.jsx
+++ client/src/components/pages/LandingPage.jsx
@@ -12,12 +12,12 @@ import {
   Heart,
   Star,
 } from "lucide-react";
-<<<<<<< HEAD:client/src/components/pages/LandingPage.jsx
+
 import { useNavigate } from "react-router-dom";
 import Header from "./Header";
-=======
 import Navbar from "../Navbar";
->>>>>>> 01d73105aa2b11379a6825c7c307b5cea8deedfd:job-portal-main/client/src/components/landingpage/Hero.jsx
+import React from "react";
+
 
  function LandingPage() {
   const steps = [
@@ -61,13 +61,11 @@ import Navbar from "../Navbar";
 
   return (
     <>
-<<<<<<< HEAD:client/src/components/pages/LandingPage.jsx
  
       <Header/>
-=======
       {/* 🔷 Hero Section */}
       <Navbar/>
->>>>>>> 01d73105aa2b11379a6825c7c307b5cea8deedfd:job-portal-main/client/src/components/landingpage/Hero.jsx
+
       <section className="w-full h-full bg-gradient-to-br from-blue-100 to-white pt-20 pb-10">
         <div className="max-w-7xl mx-auto grid md:grid-cols-2 gap-10 items-center px-4 sm:px-8">
           {/* Left */}
